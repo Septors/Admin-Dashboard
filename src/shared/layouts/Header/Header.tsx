4b3c59690498b98b4fff5f34284ab47996c3a731
Stas <savinov.stas2001@gmail.com>
2026-01-27 22:22:42 +0200

    Перша версія для партфоліо

--- src/shared/layouts/Header/Header.tsx
+++ src/shared/layouts/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import HeaderInput from "./HeaderInput";
+import HeaderInput from "./Headerinput";
 import LanguageSwitcher from "./HeaderLangSwitch";
 import UserProfile from "./HeaderProfile";
 
